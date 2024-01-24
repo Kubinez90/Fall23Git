ce16aed087e3470e0c707c83b2bf4db0a94cc8a8
Karystanbek <kubat.kozukeev@gmail.com>
2024-01-24 19:39:50 +0600

    Scrum-162/Feature-Visa Payment

---
+++ src/Payment/VisaPayment.java
@@ -0,0 +1,10 @@
+package Payment;
+
+import java.sql.SQLOutput;
+
+public class VisaPayment {
+
+    public void payByVisa(String cardHolderName, int cardNumber ){
+        System.out.println(cardHolderName+ " " + cardNumber);
+    }
+}
